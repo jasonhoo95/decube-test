6af6b3d8c62651725eae4ecd4b8f0f10405b100e
hoo chong yi <hoochongyi95@gmail.com>
2022-11-20 23:19:26 +0800

    fix some styling

--- components/cardView.tsx
+++ components/cardView.tsx
@@ -2,22 +2,24 @@ export default function CardView({ data }: any) {
 	return (
 		<div className="box-shadow mx-3 relative h-full">
 			<div className="absolute flex top-[10px] right-[10px]">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="#f5c518"
-					className="w-6 h-6">
-					<path
-						fillRule="evenodd"
-						d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-						clipRule="evenodd"
-					/>
-				</svg>
-				<span
-					className="pl-3 font-bold"
-					style={{ color: "white" }}>
-					{data.vote_average}
-				</span>
+				<div className="bg-black text-center px-3 flex">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="#f5c518"
+						className="w-6 h-6 mr-2">
+						<path
+							fillRule="evenodd"
+							d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+							clipRule="evenodd"
+						/>
+					</svg>
+					<span
+						className="text-center font-bold"
+						style={{ color: "white" }}>
+						{data.vote_average}
+					</span>
+				</div>
 			</div>
 			<img
 				src={`https://image.tmdb.org/t/p/w440_and_h660_face/${data.poster_path}`}
